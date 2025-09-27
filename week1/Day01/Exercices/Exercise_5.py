8e5509aa31a0bffb113463f0a1818757738edcc9
MedMekkaoui <mekkaouimed20@gmail.com>
2025-09-27 12:52:59 +0100

    ex05

---
+++ week1/Day01/Exercices/Exercise_5.py
@@ -0,0 +1,6 @@
+my_fav_numbers = {12,54,25,54,58,55,63}
+my_fav_numbers.add(100)
+my_fav_numbers.remove(63)
+friend_fav_numbers = {14,54,88,745,412,21}
+our_fav_numbers = my_fav_numbers | friend_fav_numbers
+print(our_fav_numbers)
