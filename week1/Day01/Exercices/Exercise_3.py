dceeb083d343915b840186031c7199f754628b2c
MedMekkaoui <mekkaouimed20@gmail.com>
2025-09-27 12:37:47 +0100

    init

---
+++ week1/Day01/Exercices/Exercise_3.py
@@ -0,0 +1,6 @@
+name = input("Hi!, what is your name ? \n")
+my_name = "Mohamed"
+if name == my_name :
+    print("ooh ! we have the same name!!")
+else :
+    print("we don't have the same name ")
---
+++ week1/Day01/Exercices/Exercise_4.py
@@ -0,0 +1,5 @@
+user_height = input("can you tell me your height : \n")
+if int(user_height) > 145 :
+    print("you are tall enough to ride")
+else : 
+    print("keep growing, you’re getting closer to the fun!")
