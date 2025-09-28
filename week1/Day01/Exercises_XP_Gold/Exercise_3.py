cbbf1834549b0edd1a64f5e3dd35e2561cfedcfe
MedMekkaoui <mekkaouimed20@gmail.com>
2025-09-28 14:40:37 +0100

    ex3

---
+++ week1/Day01/Exercises_XP_Gold/Exercise_3.py
@@ -0,0 +1,6 @@
+# Write a while loop that will continuously ask the user for their name, unless the input is equal to your name.
+my_name = "Mohamed"
+name = ""
+while my_name != name :
+    name=input("Enter your name:")
+print("You entered the correct name!")
