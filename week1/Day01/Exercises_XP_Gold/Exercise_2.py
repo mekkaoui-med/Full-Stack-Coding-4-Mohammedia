fe6b6a5836ce4eb86e4c5d9588ad2fcd57157583
MedMekkaoui <mekkaouimed20@gmail.com>
2025-09-28 14:31:17 +0100

    ex2

--- week1/Day01/Exercises_XP_Gold/Exercise_2.py
+++ week1/Day01/Exercises_XP_Gold/Exercise_2.py
@@ -1,6 +1,10 @@
-for i in range(1,21) :
-    print(i)
-#another way to do it 
-
+print()
 for i in range(1,21):
     print(i,end=" ")
+print()
+print()
+#print out every element which has an even index.
+for i in range(1,21):
+    if (i % 2 == 0) :
+        print(i)
+
