66ee4331b4fe5c708f7e90abec7429391a6b627d
MedMekkaoui <mekkaouimed20@gmail.com>
2025-09-30 12:03:59 +0100

    ex01

---
+++ week1/Day01/Exercises_XP_Ninja/Exercise_1_Outputs.py
@@ -0,0 +1,23 @@
+"""
+>>> 3 <= 3 < 9 ----> True.
+>>> 3 == 3 == 3----> True.
+>>> bool(0) ---->false.
+>>> bool(5 == "5") ----> false.
+>>> bool(4 == 4) == bool("4" == "4") ----> True.
+>>> bool(bool(None)) ----> false.
+
+>>>
+
+[
+    x = (1 == True)
+    y = (1 == False)
+    a = True + 4
+    b = False + 10
+]
+
+    X = true.
+    Y = false.
+    a = 5.
+    b = 10
+
+"""
